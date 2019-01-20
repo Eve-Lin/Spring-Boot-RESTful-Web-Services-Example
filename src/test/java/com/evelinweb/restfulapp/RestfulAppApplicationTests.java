f97fd194e2a9592c256cd12eec57b03284392149
Eve Lin <evelin.dicheva@gmail.com>
2019-01-21 01:34:04 +0200

    RESTful Web Services Example

---
+++ src/test/java/com/evelinweb/restfulapp/RestfulAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.evelinweb.restfulapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
